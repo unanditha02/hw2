f0c8149e7b13aaae67b8c5a31e7c253f2d7995e8
Nanditha Umasankar <unanditha02@gmail.com>
2022-03-25 00:15:29 -0400

    Update wsddn.py

--- wsddn.py
+++ wsddn.py
@@ -1,3 +1,4 @@
+from matplotlib.pyplot import cla
 import torch
 import torchvision
 import torch.utils.data as data
@@ -27,15 +28,32 @@ class WSDDN(nn.Module):
             print(classes)
 
         #TODO: Define the WSDDN model
-        self.features   = None
-        self.roi_pool   = None
-        self.classifier = None
+        self.features   = nn.Sequential(nn.Conv2d(3, 64, kernel_size=11,stride=4,padding=2),
+                        nn.ReLU(inplace=True),
+                        nn.MaxPool2d(kernel_size=3,stride=2,dilation=1,ceil_mode=False),
+                        nn.Conv2d(64, 192, kernel_size=5,stride=1,padding=2),
+                        nn.ReLU(inplace=True),
+                        nn.MaxPool2d(kernel_size=3,stride=2,dilation=1,ceil_mode=False),
+                        nn.Conv2d(192,384, kernel_size=3,stride=1,padding=1),
+                        nn.ReLU(inplace=True),
+                        nn.Conv2d(384, 256, kernel_size=3,stride=1,padding=1),
+                        nn.ReLU(inplace=True),
+                        nn.Conv2d(256, 256, kernel_size=3,stride=1,padding=1),
+                        nn.ReLU(inplace=True)) 
 
-        self.score_fc   = None
-        self.bbox_fc    = None
+        self.roi_pool   = torchvision.ops.RoIPool((6,6), 31)
+        # flat_dim = 
+        self.classifier = nn.Sequential(nn.Dropout(),
+                                        nn.Linear(9216, 4096), nn.ReLU(),
+                                        nn.Dropout(),
+                                        nn.Linear(4096, 4096), nn.ReLU())
+
+        self.score_fc   = nn.Linear(4096, self.n_classes)
+        self.bbox_fc    = nn.Linear(4096, self.n_classes)
 
         
         # loss
+        # self.cross_entropy = nn.CrossEntropyLoss()
         self.cross_entropy = None
 
 
@@ -52,13 +70,20 @@ class WSDDN(nn.Module):
 
         #TODO: Use image and rois as input
         # compute cls_prob which are N_roi X 20 scores
+        x = self.features(image)
+        x = self.roi_pool(x, rois)
+        # x = roi_pool(x, rois, (6, 6), 31/512)
+        x = x.view(len(rois), -1)
+
+        x = self.classifier(x)
+        score = F.softmax(self.score_fc(x), dim=1)
+        bbox = F.softmax(self.bbox_fc(x), dim=0)
         
-        
-        cls_prob = None
+        cls_prob = score * bbox
 
 
         if self.training:
-            label_vec = gt_vec.view(self.n_classes, -1)
+            label_vec = gt_vec.view(-1, self.n_classes)
             self.cross_entropy = self.build_loss(cls_prob, label_vec)
         
         return cls_prob
@@ -75,5 +100,8 @@ class WSDDN(nn.Module):
         #TODO: Compute the appropriate loss using the cls_prob that is the
         #output of forward()
         #Checkout forward() to see how it is called
-
+        image_level_scores = torch.sum(cls_prob, dim=0, keepdim=True)
+        image_level_scores = torch.clamp(image_level_scores, min=0.0, max=1.0)
+        loss = F.binary_cross_entropy(image_level_scores, label_vec, reduction="sum")
+        
         return loss
